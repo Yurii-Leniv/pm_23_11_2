1c0dda4b221d14aa230de7ec07b3a7a2b96f4a88
Yurii-Leniv <supusman32@gmail.com>
2026-04-22 14:33:40 +0300

    first lab is done

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  title = 'Лабораторна робота №1';
+  description = 'Початкове налаштування проекту та базова структура Angular-додатку';
+}
